008aa454cd4d2c641046c1449bb54443772f6be9
Brett R. Toomey <brettcwx@gmail.com>
2017-09-12 13:06:24 +0200

    Added provider

--- Sources/Audit/Provider.swift
+++ Sources/Audit/Provider.swift
@@ -1,1 +1,19 @@
+import Vapor
 
+public final class Provider: Vapor.Provider {
+    public static let repositoryName = "nodes-vapor/audit-provider"
+
+    public init() {}
+
+    public convenience init(config: Config) throws {
+        self.init()
+    }
+
+    public func boot(_ config: Config) throws {
+        config.preparations.append(AuditEvent.self)
+    }
+
+    public func boot(_ droplet: Droplet) throws {}
+
+    public func beforeRun(_ droplet: Droplet) throws {}
+}
